a772154de7ab3f620f6c70c03dccd7e58a566ffd
gabby <g.salvador@ufl.edu>
2023-12-04 10:44:14 -0500

    second

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -26,7 +26,7 @@ class SudokuGenerator:
     def __init__(self, row_length, removed_cells):
         self.row_length = row_length
         self.removed_cells = removed_cells
-        self.board = [["-" for j in range(9)] * 9]
+        self.board = [["0" for j in range(9)] * 9]
         self.box_length =  math.sqrt(self.row_length)
 
     '''
@@ -36,7 +36,7 @@ class SudokuGenerator:
 	Return: list[list]
     '''
     def get_board(self):
-        pass
+        return self.board
 
     '''
 	Displays the board to the console
@@ -46,7 +46,7 @@ class SudokuGenerator:
 	Return: None
     '''
     def print_board(self):
-        pass
+
 
     '''
 	Determines if num is contained in the specified row (horizontal) of the board
@@ -59,7 +59,10 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_row(self, row, num):
-        pass
+        for numbers in self.board[row]:
+            if num == numbers:
+                return False
+        return True
 
     '''
 	Determines if num is contained in the specified column (vertical) of the board
@@ -72,7 +75,10 @@ class SudokuGenerator:
 	Return: boolean
     '''
     def valid_in_col(self, col, num):
-        pass
+        for rows in self.board:
+            if self.board[rows][col] == num:
+                return False
+        return True
 
     '''
 	Determines if num is contained in the 3x3 box specified on the board
